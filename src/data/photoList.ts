d47e298b93274aab2280feaead77dfa7ec87b235
Zuucas <lucasmelo402@outlook.com.br>
2024-06-05 18:05:17 -0300

    refactor: project names

--- src/data/photoList.ts
+++ src/data/photoList.ts
@@ -3,19 +3,19 @@ import { Photo } from "../types/Photo";
 
 
 export const photoList: Photo[] = [
-    { id: 1, url:'/imageProjects/card-css.png' , linkVercel:'https://card-zuucas.vercel.app/', linkGithub:'https://github.com/Zuucas/Card', info:'Card feito com CSS e HTML'},
-    { id: 2, url:'/imageProjects/site-fake.png' , linkVercel:'https://site-fake-zuucas.vercel.app/', linkGithub:'https://github.com/Zuucas/Template1',info:'Site feito com CSS e HTML'},
-    { id: 3, url:'/imageProjects/page-toms.png' , linkVercel:'https://lading-page-site-toms.vercel.app/', linkGithub:'https://github.com/Zuucas/Lading_page_0',info:'Site feito com CSS e HTML'},
-    { id: 4, url:'/imageProjects/iphone.png' , linkVercel:'https://apple-market-test-zuucas.vercel.app/', linkGithub:'https://github.com/Zuucas/Lading_page_1',info:'Site feito com CSS e HTML e lógica com Javascript'},
+    { id: 1, url:'/imageProjects/card-css.png' , linkVercel:'https://card-zuucas.vercel.app/', linkGithub:'https://github.com/Zuucas/Card', info:'Card'},
+    { id: 2, url:'/imageProjects/site-fake.png' , linkVercel:'https://site-fake-zuucas.vercel.app/', linkGithub:'https://github.com/Zuucas/Template1',info:'Site Fake'},
+    { id: 3, url:'/imageProjects/page-toms.png' , linkVercel:'https://lading-page-site-toms.vercel.app/', linkGithub:'https://github.com/Zuucas/Lading_page_0',info:'Toms music'},
+    { id: 4, url:'/imageProjects/iphone.png' , linkVercel:'https://apple-market-test-zuucas.vercel.app/', linkGithub:'https://github.com/Zuucas/Lading_page_1',info:'Iphone Market'},
  
 ];
 
 
 export const photoList2: Photo[] = [
 
-    { id: 1, url:'/imageProjects/pokedex.png',linkVercel:'https://pokedex-zuucas.vercel.app/',linkGithub:'https://github.com/Zuucas/PokeDex-Zuucas',info:'React + Axios'},
-    { id: 2, url:'/imageProjects/topfilms.png',linkVercel:'https://top-films-beta.vercel.app/',linkGithub:'https://github.com/Zuucas/top-films',info:'React + Axios + React-Router'},
-    { id: 3, url:'/imageProjects/portfolio.png',linkVercel:'https://zuucas-portfolio.vercel.app/',linkGithub:'https://github.com/Zuucas/my-portfolio',info:'React'},
-    { id: 4, url:'/imageProjects/fallout-galery.png',linkVercel:'https://galeria-de-fotos-fallout.vercel.app/',linkGithub:'https://github.com/Zuucas/Galeria-de-fotos',info:'React'},
-    { id: 5, url:'/imageProjects/lista-tarefas-react.png',linkVercel:'https://task-list-zuucas-react.vercel.app/',linkGithub:'https://github.com/Zuucas/Lista_tarefas_React',info:'React'},
+    { id: 1, url:'/imageProjects/pokedex.png',linkVercel:'https://pokedex-zuucas.vercel.app/',linkGithub:'https://github.com/Zuucas/PokeDex-Zuucas',info:'PokeDex'},
+    { id: 2, url:'/imageProjects/topfilms.png',linkVercel:'https://top-films-beta.vercel.app/',linkGithub:'https://github.com/Zuucas/top-films',info:'Top Films'},
+    { id: 3, url:'/imageProjects/portfolio.png',linkVercel:'https://zuucas-portfolio.vercel.app/',linkGithub:'https://github.com/Zuucas/my-portfolio',info:'Portfolio'},
+    { id: 4, url:'/imageProjects/fallout-galery.png',linkVercel:'https://galeria-de-fotos-fallout.vercel.app/',linkGithub:'https://github.com/Zuucas/Galeria-de-fotos',info:'Gallery Photo'},
+    { id: 5, url:'/imageProjects/lista-tarefas-react.png',linkVercel:'https://task-list-zuucas-react.vercel.app/',linkGithub:'https://github.com/Zuucas/Lista_tarefas_React',info:'TaskList'},
 ]
